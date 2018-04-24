f5266df35297dfa5e7ff7f2ee84767ef9bd4a443
orionlily <476605159@qq.com>
2018-04-24 12:12:34 +0800

    SpringCloudConsumer 服务消费者(使用Ribbon方式),增加Hystrix和dashboard

--- SpringCloudConsumer/src/main/java/com/orion/SpringCloudConsumerApplication.java
+++ SpringCloudConsumer/src/main/java/com/orion/SpringCloudConsumerApplication.java
@@ -2,9 +2,11 @@ package com.orion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -15,6 +17,8 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class SpringCloudConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudConsumerApplication.class,args);
