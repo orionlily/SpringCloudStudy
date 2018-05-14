a33bbf558f1e77a28a85b9b245bd1f767dd366e4
orionlily <476605159@qq.com>
2018-05-14 18:25:27 +0800

    SpringCloudConfigServer

---
+++ SpringCloudConfigServer/src/main/java/com/orion/SpringcloudserverApplication.java
@@ -0,0 +1,12 @@
+package com.orion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudserverApplication.class, args);
+	}
+}
