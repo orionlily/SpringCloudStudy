ecd9d1dea3c056fb3043604501783ddeec7311e7
orionlily <476605159@qq.com>
2018-05-15 18:31:04 +0800

    SpringCloudConfigServer

---
+++ SpringCloudConfigServer/src/main/java/com/orion/SpringCloudServerApplication.java
@@ -0,0 +1,16 @@
+package com.orion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class SpringCloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServerApplication.class, args);
+	}
+}
