7688d7c6d4d74fad4e0c48e5d49f24d8933ec265
orionlily <476605159@qq.com>
2018-05-20 21:14:40 +0800

    SpringCloudSleuthPro消息追踪客户端Pro

--- SpringCloudSleuthPro/src/main/java/com/orion/SpringCloudSleuthProApplication.java
+++ SpringCloudSleuthPro/src/main/java/com/orion/SpringCloudSleuthProApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by Administrator on 2018/5/16.
@@ -14,5 +16,10 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 public class SpringCloudSleuthProApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudSleuthProApplication.class,args);
+    }
+
+    @Bean
+    public AlwaysSampler sampler(){
+        return  new AlwaysSampler();
     }
 }
