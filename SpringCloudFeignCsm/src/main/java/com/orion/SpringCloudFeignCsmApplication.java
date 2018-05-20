2f8a79de09be83c8c26b93ae6bc41a021f56c8d2
orionlily <476605159@qq.com>
2018-05-21 00:26:57 +0800

    SpringCloudFeignCsm服务消费者(使用Feign方式)，yml启动Hystrix,增加断路由功能以及仪表盘

--- SpringCloudFeignCsm/src/main/java/com/orion/SpringCloudFeignCsmApplication.java
+++ SpringCloudFeignCsm/src/main/java/com/orion/SpringCloudFeignCsmApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * Created by orionlily on 2018/4/23.
@@ -11,6 +13,8 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
 public class SpringCloudFeignCsmApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudFeignCsmApplication.class,args);
